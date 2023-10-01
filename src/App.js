14d560a3e4e2950a9f29b4187717ec484e2fbc12
RiddyMi <rakande02@gmail.com>
2023-10-01 17:06:33 +0100

    adding React-state-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Define the initial state with a Person object and shows boolean
+    this.state = {
+      person: {
+        fullName: 'Akande Ridwanullah',
+        bio: 'A React developer',
+        imgSrc: require("./image.jpg"),
+        profession: 'Software Engineer',
+      },
+      shows: true,
+      timeInterval: new Date (), // Initialize elapsed time to 0
+    };
+  }
+
+  // Function to toggle the 'shows' state
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows,
+    }));
+  }
+
+      // Capture the moment when the component was mounted
+  componentDidMount() {
+    this.setState({
+      timeInterval: new Date (),
+    });
+  }
+
+  // Lifecycle method to clear the timer when the component unmounts
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { shows, timeInterval } = this.state;
+
+    return (
+      <div className='App'>
+            <h1>My Profile</h1>
+            <button onClick={this.toggleShow}>
+              {shows ? "Hide Profile" : "Seek Profile"}
+            </button>
+
+            {shows && (
+              <div className='Card'>
+                <h2>{fullName}</h2>
+                <p>Bio: {bio}</p>
+                <p>Profession: {profession}</p>
+                <img src={imgSrc} alt={fullName} />
+              </div>
+            )}
+
+            <p>Time since mount: {timeInterval.toLocaleDateString()}</p>
+        </div>
+    );
+  }
 }
 
 export default App;
